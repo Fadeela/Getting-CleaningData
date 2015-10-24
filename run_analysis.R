fb6b6725b6b40e80f09959c20107a3d723cde4e0
Fadhila AlFaraj <Fadeela@users.noreply.github.com>
2015-10-24 18:39:05 +0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -16,29 +16,31 @@ data <- rbind(test, train)
 head(data)                #review the data set
 #########
 #2 Extracts only the measurements on the mean and standard deviation for each measurement. 
-library(dplyr)                  ##download the dplyr package
+library(dplyr)          ##download the dplyr package
+library(stringr)        ##download the stringr package
+library(data.table)     ##download the data.table package
+features= read.table("C:/Users/Fadhila/Desktop/Coursera/Data Science/Getting and cleaning data/Project/UCI HAR Dataset/features.txt")
 mydata <- data[,1:562]          ##remove the duplicate column of activity labels 
-
-mean_sd <- mydata %>%           ##extract the mean and standar deviation of each measurement 
-        summarise_each(funs(mean, sd), matches("V")) %>%
-        as.data.frame 
+fnames <- features %>% filter(str_detect(features$V2, "mean|std"))      ##subset to find names required
+##subset to extract the mean and standard deviation measurments 
+newdata <- mydata[,c(1,2,3,4,5,6,41,42,43,44,45,46,81,82,83,84,85,86,121,122,123,124,125,126,161,162,163,164,165,166,
+          201,202,214,215,227,228,240,241,253,253,266,267,268,269,270,271,294,295,296,346,346,347,348,349,
+          350,373,374,375,424,425,426,427,428,429,452,453,454,503,504,513,516,517,526,529,530,539,543,552,562)]
 #############
 #3 Uses descriptive activity names to name the activities in the data set
 ## get the activity_label file and name the activities 
 activity_labels= read.table("C:/Users/Fadhila/Desktop/Coursera/Data Science/Getting and cleaning data/Project/UCI HAR Dataset/activity_labels.txt")
-by_activity<-  activity_labels$V2[mydata$V1.1]  ##match data set with activity label descriptions
-mydata$V1.1 <- NULL                         
-data_activity  <- mutate(mydata, by_activity)   ##add column of the activity_label into data set after removing the numbers associated with each description  
+by_activity<-  activity_labels$V2[newdata$V1.1]  ##match data set with activity label descriptions
+newdata$V1.1 <- NULL                             ##remove unwanted extra columns
+data_activity  <- mutate(newdata, by_activity)   ##add column of the activity_label into data set after removing the numbers associated with each description  
 
 head(data_activity)
 #########
 #4 Appropriately labels the data set with descriptive variable names
-## get the features and replace the current defalut column names with new names in the features file
-library(data.table)
-features= read.table("C:/Users/Fadhila/Desktop/Coursera/Data Science/Getting and cleaning data/Project/UCI HAR Dataset/features.txt")
+## Replace the current defalut column names with new names in the features file
+colnames(data_activity)= fnames$V2               
 
-colnames(data_activity)= features$V2
-names(data_activity)[562] <- "Activity"       ##rename activity column 
+names(data_activity)[79] <- "Activity"       ##rename activity column 
 #######
 #5 From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 actgroup <- group_by(data_activity, Activity)               ## group data set by activity
